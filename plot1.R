2f0fa8eed04f491714dfadf3a9ccb4667443f5b0
JizongL <2015rpro@gmail.com>
2015-05-15 18:21:45 -0500

    first commit

--- plot1.R
+++ plot1.R
@@ -1,3 +1,4 @@
+## loading the file, this will automatically create a data file in your local directory.
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
@@ -5,13 +6,10 @@ download.file(fileUrl,destfile="./data/Dataset.zip",method="curl")
 ## Unzip the file folder downloaded into the "data" folder. 
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 
-dataPath <- "./data/household_power_consumption.txt"
-Data <- read.table(dataPath, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+data <- "./data/household_power_consumption.txt"
+Data <- read.table(data, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- Data[Data$Date %in% c("1/2/2007","2/2/2007") ,]
 
-head(subSetData)
-
-#str(subSetData)
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot1.png", width=480, height=480)
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
